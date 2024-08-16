5ed5d18354f6574e63ad09114eec1a8684418113
Haoyang Gao <gaohaoyang126@126.com>
2024-08-16 12:20:47 -0400

    feat: 🎸 update

--- src/content.tsx
+++ src/content.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './content.css';
 import App from './App';
 
 const root = document.createElement('div');
