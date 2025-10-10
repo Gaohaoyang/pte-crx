04bc2de8704daa4c78f0b55a1f3ddeddc1e55c01
Haoyang Gao <gaohaoyang126@gmail.com>
2025-10-10 15:16:20 -0400

    feat: enhance ProgressBar component with smooth transition effect and internal state management

--- src/ContentUI/ProgressBar/index.tsx
+++ src/ContentUI/ProgressBar/index.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from 'react'
+
 type Props = {
   progress: number
   total?: number
 }
 const ProgressBar = (props: Props) => {
   const { progress, total = 90 } = props
+  const [progressInner, setProgressInner] = useState(0)
+  useEffect(() => {
+    setTimeout(() => {
+      setProgressInner(progress)
+    }, 500)
+  }, [progress])
   return (
-    <div className="h-1 w-full rounded-sm bg-slate-300">
+    <div className="h-1 w-full rounded-full bg-slate-300 relative overflow-hidden">
       <div
-        className="h-1 rounded-sm bg-cyan-600"
-        style={{ width: `${(progress / total) * 100}%` }}
+        className="duration-700 ease-out absolute -left-full top-0 h-1 w-full rounded-full bg-cyan-600 transition-transform"
+        style={{ transform: `translateX(${(progressInner / total) * 100}%)` }}
       ></div>
     </div>
   )
